9c1b7e66f3a99383b1f6e41c1f89b4be8de6f865
bwuni <bwuni@outlook.com>
2019-10-03 00:47:51 +0800

    first commit

---
+++ packages/zug-app/src/base/login/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './index.scss'
+import bgImg from './bg.jpg'
+
+export default () => {
+    const [isShow,setIsShow] = useState(false) 
+
+    document.body.onclick = ()=>setIsShow(!isShow)
+
+    return <div className={isShow?"login":"login hide"} style={{ backgroundImage: `url(${bgImg})`,backgroundSize:'100% 100%'}}>
+        <div className="login-input-panel"></div>
+
+    </div>
+}
